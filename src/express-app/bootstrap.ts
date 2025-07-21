5898d18b2ae885ad1542783319db084d1d5ec7ab
Priapisman677 <expeliamm02@hotmail.com>
2025-07-21 09:33:16 -0700

    update

--- src/express-app/bootstrap.ts
+++ src/express-app/bootstrap.ts
@@ -16,7 +16,7 @@ const setENV = ()=>{
 
 
      //* Check for required environment variables or throw stop execution.   
-    const requiredVars = ['PORT', 'JWT_SECRET']
+    const requiredVars = [ 'JWT_SECRET']
 
     requiredVars.forEach((varName)=>{
         if(!process.env[varName]){
